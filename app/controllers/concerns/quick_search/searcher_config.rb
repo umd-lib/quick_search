1881667d9e3b4c869b227d5ff608b46ebfefb5ee
Kevin Beswick <kdbeswic@ncsu.edu>
2016-04-18 10:11:13 -0400

    remove website searcher; fix JS loading for themes; fix search config loading

--- app/controllers/concerns/quick_search/searcher_config.rb
+++ app/controllers/concerns/quick_search/searcher_config.rb
@@ -4,8 +4,13 @@ module QuickSearch::SearcherConfig
   private
 
   def searcher_config(searcher)
-    #TODO: test for the existence of this?
-    "QuickSearch::Engine::#{searcher.upcase}_CONFIG".constantize
+    # Best Bets searcher is available in QuickSearch core, use app_config
+    if searcher == 'best_bets'
+      QuickSearch::Engine::APP_CONFIG['best_bets']
+    else
+      #TODO: test for the existence of this?
+      "QuickSearch::Engine::#{searcher.upcase}_CONFIG".constantize
+    end
   end
 
 end

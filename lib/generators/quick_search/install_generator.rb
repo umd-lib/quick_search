dfa4cfcd7aff78dedc3c3d11d31f94053a57259c
Kevin Beswick <kdbeswic@ncsu.edu>
2016-04-26 09:13:03 -0400

    ask whether to install searchers / theme in generator

--- lib/generators/quick_search/install_generator.rb
+++ lib/generators/quick_search/install_generator.rb
@@ -11,10 +11,12 @@ ROUTES
         insert_into_file "config/routes.rb", routes, :after => "Rails.application.routes.draw do\n"
       end
 
+
       desc 'insert generic theme'
       def insert_theme
+
         theme_gem_entry = <<-THEME
-\n\n
+\n
 # -Inserted by QuickSearch-
 
 # QuickSearch theme
@@ -22,6 +24,30 @@ ROUTES
 # Remove the following if you want to use a different theme
 
 gem 'quick_search-generic_theme'
+
+# END -Inserted by QuickSearch-
+
+THEME
+
+
+        say("\nYou'll need to include a theme which QuickSearch uses to render your results. We've built a generic
+theme which can be modified or extended to meet your needs. Alternatively you can build your own from
+scratch and include it.\n\n")
+
+        @theme_response = ask("Would you like to install the generic theme?", limited_to: ['y', 'n'])
+
+        if @theme_response == 'y'
+          insert_into_file "Gemfile", theme_gem_entry, :after => /gem [\"\']quick_search-core[\"\'].*$/
+        end
+
+      end
+
+
+      desc 'insert searchers'
+      def insert_searchers
+        searcher_gem_entry = <<-SEARCHERS
+\n
+# -Inserted by QuickSearch-
 
 # QuickSearch searchers
 #
@@ -36,9 +62,22 @@ gem 'quick_search-placeholder_searcher'
 
 # -END Inserted by QuickSearch-
 
-        THEME
-        insert_into_file "Gemfile", theme_gem_entry, :after => /gem [\"\']quick_search-core[\"\'].*$/
+        SEARCHERS
+
+        say("\nTo have a working application you'll need to install some searchers. Searchers are the
+code that integrates with third-party APIs, handles performing searches and creating results objects.
+By default quick_search-core provides no searchers. Some searchers are available and it is easy to
+write your own.\n\n")
+
+
+        @searcher_response = ask("Would you like to install some basic searchers that do not require API keys?", limited_to: ['y', 'n'])
+
+        if @searcher_response == 'y'
+          insert_into_file "Gemfile", searcher_gem_entry, :after => /gem [\"\']quick_search-core[\"\'].*$/
+        end
+
       end
+
 
       desc 'bundle install theme/searchers'
       def bundle_install_theme_searchers
@@ -76,6 +115,13 @@ gem 'quick_search-placeholder_searcher'
 
       desc 'display messages about what needs to be configured'
       def configuration_messages
+        say "QuickSearch installation complete.\n\n", :green
+        if @searcher_response == 'y'
+          say "We've installed a set of default searchers.\n", :green
+        end
+        if @theme_response == 'y'
+          say "We've installed the generic theme.\n", :green
+        end
         file = File.read(File.join( File.expand_path('../templates', __FILE__), 'post_install.txt'))
         say file, :green
       end

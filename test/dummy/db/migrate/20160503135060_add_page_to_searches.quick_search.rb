bd5bc08e148e189192716db9df8cdafbb4475b78
Kevin Beswick <kdbeswic@ncsu.edu>
2016-05-03 14:49:13 -0400

    remove dummy app migrations

--- test/dummy/db/migrate/20160503135060_add_page_to_searches.quick_search.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from quick_search (originally 20140225145441)
-class AddPageToSearches < ActiveRecord::Migration
-  def change
-    add_column :searches, :page, :string
-  end
-end

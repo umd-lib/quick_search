3900a6d226151a9901232c40427d81f70e5cff4a
Kevin Beswick <kdbeswic@ncsu.edu>
2016-05-03 16:06:04 -0400

    add dummy test app schema.rb

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,6 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140225145441) do
+
+  create_table "events", force: true do |t|
+    t.string   "category"
+    t.string   "action"
+    t.string   "label"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "searches", force: true do |t|
+    t.string   "query"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "page"
+  end
 
 end
